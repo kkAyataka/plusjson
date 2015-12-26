9f9202e93854ddf5d1b48d580ceaad82c5246531
kkAyataka <kk.ayataka@gmail.com>
2015-12-26 10:26:39 +0900

    Add test for copy constructor, =, == and !- operator.

--- src/unit_test/test-plusjson.cpp
+++ src/unit_test/test-plusjson.cpp
@@ -64,3 +64,69 @@ TEST(JSON, value__value) {
     v1.get<plusjson::Number>() = 2;
     ASSERT_NE(v2, v1);
 }
+
+// Copy constructor, =, == and != operator
+
+namespace {
+
+template<class T>
+void test_copy_and_eq(const T raw1, const T raw2) {
+    plusjson::Value v1(raw1);
+
+    plusjson::Value v2(v1);
+    plusjson::Value v3;
+    v3 = v2;
+
+    ASSERT_EQ(v1, v2);
+    ASSERT_EQ(v1, v3);
+
+    v1.get<T>() = raw2;
+
+    ASSERT_NE(v1, v2);
+    ASSERT_NE(v1, v3);
+}
+
+} // no namespace
+
+TEST(JSON, copy_and_ep_object) {
+    plusjson::Object o1;
+    o1["key"] = plusjson::Value("Value");
+
+    plusjson::Object o2;
+    o2["key1"] = plusjson::Value("Value1");
+
+    test_copy_and_eq(o1, o2);
+}
+
+TEST(JSON, copy_and_eq_array) {
+    plusjson::Array a1;
+    a1.push_back(plusjson::Value(1.0));
+
+    plusjson::Array a2;
+
+    test_copy_and_eq(a1, a2);
+}
+
+TEST(JSON, copy_and_ep_number) {
+    test_copy_and_eq(123.4, -1.1);
+}
+
+TEST(JSON, copy_and_ep_string) {
+    const plusjson::String s1 = "StringA";
+    const plusjson::String s2 = "I'm lovin' it";
+
+    test_copy_and_eq(s1, s2);
+}
+
+TEST(JSON, copy_and_eq_boolean) {
+    test_copy_and_eq(true, false);
+}
+
+TEST(JSON, copy_and_eq_null) {
+    const plusjson::Value v1;
+    const plusjson::Value v2;
+    const plusjson::Value v3(1.0);
+
+    ASSERT_EQ(v1, v2);
+    ASSERT_NE(v1, v3);
+}
